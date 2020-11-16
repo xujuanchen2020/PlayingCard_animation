0a946d5156c9011978b35646578455f8b9e786ec
Xujuan Chen <xujuan.chen@lc.cuny.edu>
2020-11-16 12:06:06 -0500

    push to center

--- PlayingCard_practice/CardBehavior.swift
+++ PlayingCard_practice/CardBehavior.swift
@@ -26,7 +26,21 @@ class CardBehavior: UIDynamicBehavior {
     
     private func push(_ item: UIDynamicItem) {
         let push = UIPushBehavior(items: [item], mode: .instantaneous)
-        push.angle = (2*CGFloat.pi).arc4random
+        if let referenceBounds = dynamicAnimator?.referenceView?.bounds {
+            let center = CGPoint(x: referenceBounds.midX, y: referenceBounds.midY)
+            switch (item.center.x, item.center.y) {
+            case let (x, y) where x < center.x && y < center.y:
+                push.angle = (CGFloat.pi/2).arc4random
+            case let (x, y) where x > center.x && y < center.y:
+                push.angle = CGFloat.pi-(CGFloat.pi/2).arc4random
+            case let (x, y) where x < center.x && y > center.y:
+                push.angle = (-CGFloat.pi/2).arc4random
+            case let (x, y) where x > center.x && y > center.y:
+                push.angle = CGFloat.pi+(CGFloat.pi/2).arc4random
+            default:
+                push.angle = (CGFloat.pi*2).arc4random
+            }
+        }
         push.magnitude = CGFloat(1.0) + CGFloat(2.0).arc4random
         push.action = {[unowned push, weak self] in
             self?.removeChildBehavior(push)
