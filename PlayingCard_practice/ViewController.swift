8f1008ea86a3990b403408ed42fbdd64e01fa325
Xujuan Chen <xujuan.chen@lc.cuny.edu>
2020-11-16 10:17:04 -0500

    add animation of matched cards

--- PlayingCard_practice/ViewController.swift
+++ PlayingCard_practice/ViewController.swift
@@ -38,6 +38,12 @@ class ViewController: UIViewController {
         return cardViews.filter{ $0.isFaceUp && !$0.isHidden}
     }
     
+    private var faceUpCardViewsMatch: Bool {
+        return faceUpCardViews.count == 2 &&
+            faceUpCardViews[0].rank == faceUpCardViews[1].rank &&
+            faceUpCardViews[0].suit == faceUpCardViews[1].suit
+    }
+    
     @objc func flipCard(_ recognizer: UITapGestureRecognizer) {
         switch recognizer.state {
         case .ended:
@@ -50,7 +56,37 @@ class ViewController: UIViewController {
                     chosenCardView.isFaceUp = !chosenCardView.isFaceUp
                 },
                     completion: { finished in
-                        if self.faceUpCardViews.count == 2 {
+                        if self.faceUpCardViewsMatch {
+                            UIViewPropertyAnimator.runningPropertyAnimator(
+                                withDuration: 0.6,
+                                delay: 0,
+                                options: [],
+                                animations: {
+                                    self.faceUpCardViews.forEach{
+                                        $0.transform = CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0)
+                                    }
+                            },
+                                completion: { position in
+                                    UIViewPropertyAnimator.runningPropertyAnimator(
+                                        withDuration: 0.75,
+                                        delay: 0,
+                                        options: [],
+                                        animations: {
+                                            self.faceUpCardViews.forEach{
+                                                $0.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
+                                                $0.alpha = 0
+                                            }
+                                    },
+                                        completion: { position in
+                                            self.faceUpCardViews.forEach{
+                                                $0.isHidden = true
+                                                $0.alpha = 1
+                                                $0.transform = .identity
+                                            }
+                                    })
+                            })
+                        }
+                        else if self.faceUpCardViews.count == 2 {
                             self.faceUpCardViews.forEach{cardView in
                                 UIView.transition(
                                     with: cardView,
