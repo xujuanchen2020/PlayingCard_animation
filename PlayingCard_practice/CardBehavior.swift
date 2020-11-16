c0c0227944afcafad76e5036fd501a42212c2236
Xujuan Chen <xujuan.chen@lc.cuny.edu>
2020-11-16 11:49:21 -0500

    add class UIDynamicBehavior

---
+++ PlayingCard_practice/CardBehavior.swift
@@ -0,0 +1,54 @@
+//
+//  CardBehavior.swift
+//  PlayingCard_practice
+//
+//  Created by qiurong chen on 11/16/20.
+//  Copyright © 2020 Xujuan Chen. All rights reserved.
+//
+
+import UIKit
+
+class CardBehavior: UIDynamicBehavior {
+    
+    lazy var collisionBehavior: UICollisionBehavior = {
+        let behavior = UICollisionBehavior()
+        behavior.translatesReferenceBoundsIntoBoundary = true
+        return behavior
+    }()
+    
+    lazy var itemBehavior: UIDynamicItemBehavior = {
+        let behavior = UIDynamicItemBehavior()
+        behavior.allowsRotation = false
+        behavior.elasticity = 1.0
+        behavior.resistance = 0
+        return behavior
+    }()
+    
+    private func push(_ item: UIDynamicItem) {
+        let push = UIPushBehavior(items: [item], mode: .instantaneous)
+        push.angle = (2*CGFloat.pi).arc4random
+        push.magnitude = CGFloat(1.0) + CGFloat(2.0).arc4random
+        push.action = {[unowned push, weak self] in
+            self?.removeChildBehavior(push)
+        }
+        addChildBehavior(push)
+    }
+    
+    func addItem(_ item: UIDynamicItem) {
+        collisionBehavior.addItem(item)
+        itemBehavior.addItem(item)
+        push(item)
+    }
+    
+    override init() {
+        super.init()
+        addChildBehavior(collisionBehavior)
+        addChildBehavior(itemBehavior)
+    }
+    
+    convenience init(in animator: UIDynamicAnimator) {
+        self.init()
+        animator.addBehavior(self)
+    }
+
+}
--- PlayingCard_practice/ViewController.swift
+++ PlayingCard_practice/ViewController.swift
@@ -16,21 +16,7 @@ class ViewController: UIViewController {
     
     lazy var animator = UIDynamicAnimator(referenceView: view)
     
-    lazy var collisionBehavior: UICollisionBehavior = {
-        let behavior = UICollisionBehavior()
-        behavior.translatesReferenceBoundsIntoBoundary = true
-        animator.addBehavior(behavior)
-        return behavior
-    }()
-    
-    lazy var itemBehavior: UIDynamicItemBehavior = {
-        let behavior = UIDynamicItemBehavior()
-        behavior.allowsRotation = false
-        behavior.elasticity = 1.0
-        behavior.resistance = 0
-        animator.addBehavior(behavior)
-        return behavior
-    }()
+    lazy var cardBehavior = CardBehavior(in: animator)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,15 +35,8 @@ class ViewController: UIViewController {
             cardView.rank = card.rank.order
             cardView.suit = card.suit.rawValue
             cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
-            collisionBehavior.addItem(cardView)
-            itemBehavior.addItem(cardView)
-            let push = UIPushBehavior(items: [cardView], mode: .instantaneous)
-            push.angle = (2*CGFloat.pi).arc4random
-            push.magnitude = CGFloat(1.0) + CGFloat(2.0).arc4random
-            push.action = {[unowned push] in
-                push.dynamicAnimator?.removeBehavior(push)
-            }
-            animator.addBehavior(push)
+            cardBehavior.addItem(cardView)
+
         }
     }
     
